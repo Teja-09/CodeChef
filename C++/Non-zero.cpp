b3aad7a299600220f9ca28aa330d38f5aae156bc
Krish_09 <krishnateja.tumma@gmail.com>
2020-02-25 22:30:22 +0530

    added Add odd or sub even
    Signed-off-by: Krish_09 <krishnateja.tumma@gmail.com>

---
+++ C++/Non-zero.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int t, n, i;
+	cin >> t;
+	while(t--)
+	{
+		cin >> n;
+		vector< int >a(n);
+		for(i=0; i<n; i++)
+		{
+			cin >> a[i];
+		}
+		int sum = 0;
+		for(i=0; i<n; i++)
+		{
+			if(a[i] > )
+		}														
+	}
+}
