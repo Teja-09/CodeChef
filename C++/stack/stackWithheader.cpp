dde4a46a8bb97b57bc127623f5752b82efe09fb6
Krish_09 <krishnateja.tumma@gmail.com>
2020-03-21 10:01:56 +0530

    StackwithHeader

---
+++ C++/stack/stackWithheader.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main()
+{
+	stack <int> s;
+	s.push(2);
+	s.push(4);
+	s.push(1);
+	
+	int len = s.size();
+	
+	for(int i=0;i<len;i++)
+	{
+		cout << s.top() << "  ";
+		s.pop();
+	}
+	return 0;
+
+}
+
