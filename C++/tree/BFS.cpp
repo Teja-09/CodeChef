1be33acae27c64c11d16d16d7d58ec31cbb4f89e
Krish_09 <krishnateja.tumma@gmail.com>
2020-03-22 15:25:43 +0530

    BFS in tree

---
+++ C++/tree/BFS.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class node
+{
+	public:
+		int data;
+		node* left;
+		node* right;
+		node(int data)
+		{
+			this->data = data;
+			left = NULL;
+			right = NULL;
+		}
+};
+
+node* newnode(int data)
+{
+	node* newnode = new node(data);
+	return newnode;
+}
+
+void BFS(node* root)
+{
+	if(root == NULL)
+	{
+		cout << 0;
+	}
+	
+	queue<node*>q;
+	q.push(root);
+	
+	while(!q.empty())
+	{
+		int siz = q.size();
+		
+		while(siz--)
+		{
+			node* val = q.front();
+			q.pop();
+			
+			cout << val->data << " ";
+			if(val->left != NULL)
+			{
+				q.push(val->left);
+			}
+			
+			if(val->right != NULL)
+			{
+				q.push(val->right);
+			}
+			
+		}
+	}
+	
+	
+}
+
+int main()
+{
+	node* root = newnode(3);
+	root->left = newnode(2);
+	root->right = newnode(1);
+	BFS(root);
+
+}
+
